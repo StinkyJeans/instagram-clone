69e7038fee6a1a5d62d059b1378b89d8c917c2b7
StinkyJeans <johncona3@gmail.com>
2023-10-25 23:43:50 +0800

    add instagram logo to the leftside of the header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import Header from '../../components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +15,7 @@ export default function Home() {
         <link rel='icon' href="/favicon.ico"/>
       </Head>
       {/* Header */}
-
+        <Header />
       {/* Feed */}
 
       {/* Modal */}
