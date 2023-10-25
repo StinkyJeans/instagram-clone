be88113e404b14261967117e1745065a58750aed
StinkyJeans <johncona3@gmail.com>
2023-10-25 23:19:09 +0800

    adding all

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>
+           Instagram Clone
+        </title> 
+        <meta name='description' content='general'/>
+        <link rel='icon' href="/favicon.ico"/>
+      </Head>
+      {/* Header */}
+
+      {/* Feed */}
+
+      {/* Modal */}
+    </div>
+  )
+}
