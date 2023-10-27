ccb40e6001107c3c45bb8d8a426911cffc50ef9b
StinkyJeans <johncona3@gmail.com>
2023-10-27 21:53:20 +0800

    style the story section and install tailwind scrollbar

--- components/Stories.js
+++ components/Stories.js
@@ -17,19 +17,11 @@ export default function Stories() {
                 id: inc,
             })
         }
-
-
-        // const storyUsers = faker.array(20,(i)=>({
-        //     username: ({locale:"en"}),
-        //     img: `https://i.pravatar.cc/150?img=${Math.ceil(Math.random * 70)}`,
-        //     id: i,
-            
-        // }));
-        setStoryUsers(storyUsers)
+         setStoryUsers(storyUsers)
         console.log(storyUsers)
     },[]);
   return (
-    <div>
+    <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none'>
         {storyUsers.map(user=>(
             <Story key={user.id} username={user.username} img={user.img}/>
         ))}
