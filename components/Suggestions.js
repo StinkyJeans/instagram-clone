e38007cdb0ac620db577ce452ba1eb1f8f01b9b4
StinkyJeans <johncona3@gmail.com>
2023-11-01 04:05:26 +0800

    updated suggestions key

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -22,7 +22,7 @@ export default function Suggestions() {
 
   return (
     <div className='mt-4 ml-10'>
-        <div className='flex justify-between mb-5 text-sm'>
+        <div key={suggestions.id} className='flex justify-between mb-5 text-sm'>
             <h3 className='font-bold text-gray-400'>Suggestions for you</h3>
             <button className='text-gray-600 font-semibold'>See all</button>
         </div>
